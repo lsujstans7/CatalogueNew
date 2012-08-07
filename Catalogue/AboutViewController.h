50f3ad4c1ffac7177779e1661af048521a303a19
lsujstans7 <jstans7@lsu.edu>
2012-08-07 10:15:11 -0500

    class

---
+++ Catalogue/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Catalogue
+//
+//  Created by iOS Camp on 8/7/12.
+//  Copyright (c) 2012 kcfancher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
